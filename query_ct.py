4b76bc1e3503cadfc706cc77c55a6469e7d8e194
Pontus Palmenäs <pontus@palmenas.se>
2020-06-04 00:29:07 +0200

    Rename query_ct.py

--- query_ct.py
+++ query_ct.py
@@ -2,6 +2,13 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+# open bolaget.json
+# for each entry
+# query cellartracker and parse score
+# add score to object
+# save as ctbolaget.json
+
+
 #base_url = 'https://www.cellartracker.com/list.asp?fInStock=0&Table=List&iUserOverride=0&Vintage=2010&Wine=Henri+Goutorbe+Champagne+Grand+Cru+Brut+Mill%E9sim%E9'
 #response = requests.get(base_url)
 #print(response.status_code)
